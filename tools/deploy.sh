21d275d13a1c30726288068904e931b0789c75c1
Maycon Pacheco <mayconjrpacheco@gmail.com>
2022-07-24 02:15:10 -0300

    Update deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -64,6 +64,7 @@ test() {
 }
 
 resume_site_dir() {
+  pip install jupyter nbconvert
   cp -R _notebooks "$SITE_DIR"/notebooks
   if [[ -n $_baseurl ]]; then
     # Move the site file to the regular directory '_site'
